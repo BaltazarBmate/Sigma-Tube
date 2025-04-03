709e9fb34ffc30fc68b6bdcb125cb02653dbc985
BaltazarBmate <baltazar.leon@bmateglobal.com>
2025-04-03 12:48:04 -0600

    Change: Files location Files locations changed to set up the page navigation system

--- Main.py
+++ Main.py
@@ -1,29 +1,21 @@
 import streamlit as st
 
-st.title("📂 Main Page - Inventory Management")
+# --- PAGE SETUP ---
+ROP_page = st.Page(
+    page="views/1_ROP.py",
+    title="ROP",
+    default=True,
+)
 
-st.write("Select an application to launch:")
+Dashboard_page = st.Page(
+    page="views/2_Dashboard.py",
+    title="Dashboard",
+)
 
-col1, col2 = st.columns(2)
+# --- Navigation Setup ---
 
-with col1:
-    if st.button("📊 Open Inventory Dashboard"):
-        st.query_params(page="dashboard")
-        st.write("Redirecting to Dashboard...")
-        st.experimental_rerun()
+pg = st.navigation(pages=[ROP_page,Dashboard_page])
 
-with col2:
-    if st.button("📦 Open Full Inventory Report"):
-        st.query_params(page="full_report")
-        st.write("Redirecting to Full Inventory Report...")
-        st.experimental_rerun()
 
-# Check the query parameters to determine which page to load
-query_params = st.experimental_get_query_params()
-
-if query_params.get("page") == ["dashboard"]:
-    exec(open("Dashboard.py").read())
-elif query_params.get("page") == ["full_report"]:
-    exec(open("import_streamlit_as_st.py").read())
-else:
-    st.write("Please select an application from the buttons above.")
+# --- Run Navigation ---
+pg.run()
